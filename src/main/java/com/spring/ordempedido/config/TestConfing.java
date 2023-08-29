910ae4d93993fb3bc295f316b8153c6dd312e878
MauroJRamos <mauroramosti@gmail.com>
2023-08-29 15:49:12 -0300

    Rua Otacílio Novais, 172 – Jacarepaguá

--- src/main/java/com/spring/ordempedido/config/TestConfing.java
+++ src/main/java/com/spring/ordempedido/config/TestConfing.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.spring.ordempedido.entities.Category;
 import com.spring.ordempedido.entities.Order;
 import com.spring.ordempedido.entities.OrderItem;
+import com.spring.ordempedido.entities.Payment;
 import com.spring.ordempedido.entities.Product;
 import com.spring.ordempedido.entities.User;
 import com.spring.ordempedido.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfing implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T20:53:07Z"),o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
