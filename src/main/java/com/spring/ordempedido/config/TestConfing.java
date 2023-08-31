f753db2fae38cd24e4da1ebc53ddd2efb247398c
MauroJRamos <mauroramosti@gmail.com>
2023-08-31 15:15:35 -0300

    alter project

--- src/main/java/com/spring/ordempedido/config/TestConfing.java
+++ src/main/java/com/spring/ordempedido/config/TestConfing.java
@@ -84,6 +84,7 @@ public class TestConfing implements CommandLineRunner {
 		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T20:53:07Z"),o1 );
 		o1.setPayment(pay1);
 		
+		
 		orderRepository.save(o1);
 	}
 
